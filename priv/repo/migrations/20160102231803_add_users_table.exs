df7b7038233023cb4f6cdef78fe2122983855f48
Brian Marick <marick@exampler.com>
2016-01-03 15:00:56 -0600

    Simple users table

---
+++ priv/repo/migrations/20160102231803_add_users_table.exs
@@ -0,0 +1,16 @@
+defmodule Critter4us.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+      add :role, :string
+
+      timestamps
+    end
+
+    create unique_index :users, [:email]
+  end
+end
