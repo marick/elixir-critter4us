b896156770dd5c2a465f2f8783bf942927729aac
Brian Marick <marick@exampler.com>
2016-01-06 17:58:42 -0600

    Need to stop futzing around with authentication

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,24 @@
 defmodule Critter4us.PageControllerTest do
   use Critter4us.ConnCase
 
-  test "GET /" do
-    conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Login"
+  @index_path page_path(conn(), :index)
+
+  setup do
+    conn = authenticated()
+    {:ok, authenticated: conn}
+  end
+
+  ## These tests do not actually capture what's required to make logging in useful.
+  ## 'Cause it's currently not.
+
+  test "GET / when not logged in redirects" do
+    unauthorized_conn = conn()
+    result = get(unauthorized_conn, @index_path)
+    assert redirected_to(result) == "/login"
+  end
+
+  test "GET / when logged in", %{authenticated: conn}  do
+    conn = get conn, "/"
+    assert html_response(conn, 200)
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,21 @@
 defmodule Critter4us.PageController do
   use Critter4us.Web, :controller
+  require Logger
+  import Openmaize.AccessControl
 
   plug Openmaize.Login when action in [:login_user]
   plug Openmaize.Logout when action in [:logout]
 
+#  plug :authorize, roles: ["superuser", "admin", "requester"]
+
   def index(conn, _params) do
-    render conn, "index.html"
+    if conn.assigns.current_user do
+      Logger.info(inspect conn.assigns)
+      render conn, "index.html"
+    else
+      Logger.info("NUIL NBUL NLI")
+      redirect(conn, to: "/login")
+    end
   end
 
   def login(conn, _params) do
