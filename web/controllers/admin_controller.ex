ba123b0fc057a99191a9960a03cc231585575c8d
Brian Marick <marick@exampler.com>
2016-01-06 17:53:47 -0600

    don’t forget superuser

--- web/controllers/admin_controller.ex
+++ web/controllers/admin_controller.ex
@@ -1,10 +1,9 @@
 defmodule Critter4us.AdminController do
   use Critter4us.Web, :controller
-
   import Openmaize.AccessControl
   alias Critter4us.User
 
-  plug :authorize, roles: ["admin"]
+  plug :authorize, roles: ["superuser", "admin"]
 
   def index(conn, _params) do
     render(conn, "index.html")
