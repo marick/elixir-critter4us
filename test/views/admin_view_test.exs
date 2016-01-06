072700074a9b649d5f9b3929e172d41975cbc813
Brian Marick <marick@exampler.com>
2016-01-06 17:22:42 -0600

    Some boilerplate

---
+++ test/views/admin_view_test.exs
@@ -0,0 +1,5 @@
+defmodule Critter4us.AdminViewTest do
+  use Critter4us.ConnCase, async: true
+
+  IO.puts "== Should there be AdminView tests?"
+end
--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,5 @@
 defmodule Critter4us.PageViewTest do
   use Critter4us.ConnCase, async: true
+
+  IO.puts "== Should there be PageView tests?"
 end
