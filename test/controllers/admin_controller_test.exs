7dc1c9d8a82ce31a3305951e82d7d9c633a5196e
Brian Marick <marick@exampler.com>
2016-01-06 17:04:56 -0600

    admin controller tests

---
+++ test/controllers/admin_controller_test.exs
@@ -0,0 +1,22 @@
+defmodule Critter4us.AdminControllerTest do
+  use Critter4us.ConnCase
+  require Critter4us.AdminController
+
+  @index_path admin_path(conn(), :index)
+
+  setup do
+    conn = authenticated()
+    {:ok, authenticated: conn}
+  end
+
+  test "GET / when not logged in redirects" do
+    unauthorized_conn = conn()
+    result = get(unauthorized_conn, @index_path)
+    assert redirected_to(result) == "/login"
+  end
+
+  test "GET / when logged in", %{authenticated: conn}  do
+    conn = get conn, "/"
+    assert html_response(conn, 200)
+  end
+end
