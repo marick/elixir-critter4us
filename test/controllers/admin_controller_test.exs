cf0502b89f7ba406d1ce016b085c9afe1d6bb405
Brian Marick <marick@exampler.com>
2016-01-08 15:50:08 -0600

    Avoid intermittent test failures.

--- test/controllers/admin_controller_test.exs
+++ test/controllers/admin_controller_test.exs
@@ -12,11 +12,13 @@ defmodule Critter4us.AdminControllerTest do
   test "GET / when not logged in redirects" do
     unauthorized_conn = conn()
     result = get(unauthorized_conn, @index_path)
+    # IO.inspect result
     assert redirected_to(result) == "/login"
   end
 
   test "GET / when logged in", %{authenticated: conn}  do
     conn = get conn, "/"
+    # IO.inspect conn
     assert html_response(conn, 200)
   end
 end
--- test/support/conn_test_helpers.ex
+++ test/support/conn_test_helpers.ex
@@ -5,7 +5,10 @@ defmodule Critter4us.ConnTestHelpers do
   def authenticated() do 
     {:ok, user_token} = %{id: 2, login: "derp", role: "admin"} |> Token.generate_token({0, 86400})
 
-    conn()
+    result = conn()
     |> put_req_cookie("access_token", user_token)
+
+    :timer.sleep(1)  # Temp this is required to avoid intermittent test failures.
+    result
   end
 end
