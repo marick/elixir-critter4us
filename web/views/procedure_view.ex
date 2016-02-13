5286dae603d0477ccc1e07e7ed74e61c101e3067
Brian Marick <marick@exampler.com>
2016-02-13 17:13:08 -0600

    Sort procedures

--- web/views/procedure_view.ex
+++ web/views/procedure_view.ex
@@ -1,3 +1,7 @@
 defmodule Critter4us.ProcedureView do
   use Critter4us.Web, :view
+
+  def sorted_procedures(procedures) do
+    Enum.sort(procedures, &(String.downcase(&1.name) < String.downcase(&2.name)))
+  end
 end
